9b3ccc89abc8b26ccc2dce50d6f7e9f926327cf2
Corbin Taylor <cptaylor38@yahoo.com>
2020-05-15 19:31:50 -0400

    fnd

--- portfolio/sendGrid/sendGrid.js
+++ portfolio/sendGrid/sendGrid.js
@@ -10,14 +10,23 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const router = express.Router();
 app.post('/.netlify/functions/sendGrid/email', async (req, res) => {
+  let email = req.body.email;
+  let subject = req.body.subject;
+  let message = req.body.message;
+
   const msg = {
     to: 'cylor.dev@gmail.com',
     from: 'cylor.dev@gmail.com',
-    subject: `${req.body.email}`,
-    text: `${req.body.message}`,
-    html: `<h3>${req.body.email}</h3>
-                    <h4>${req.body.subject}</h4>
-                    <p>${req.body.message}</p>`,
+    subject: `Portfolio alert`,
+    text: `Someone has sent you a message from your portfolio.`,
+    html: `<h3>${email}</h3>
+                    <h4>${subject}</h4>
+                    <p>${message}</p>`,
+    dynamic_template_data: {
+      email: email,
+      subject: subject,
+      message: message,
+    },
   };
 
   try {
