27b0a2d52ee9a495abfafccf7a46ad8eb571e085
Corbin Taylor <cptaylor38@yahoo.com>
2020-05-15 17:09:38 -0400

    fixing netlify deploy

--- portfolio/sendGrid/sendGrid.js
+++ portfolio/sendGrid/sendGrid.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('dotenv');
 const serverless = require('serverless-http');
 const app = express();
@@ -7,7 +8,7 @@ const sgMail = require('@sendgrid/mail');
 const PORT = process.env.PORT || 5000;
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-app.use(bodyParser);
+const router = express.Router();
 
 app.post('/contact/email', async (req, res) => {
   try {
@@ -26,14 +27,19 @@ app.post('/contact/email', async (req, res) => {
   }
 });
 
-app.all('*', (req, res) => {
-  console.log(req.path);
-  console.log('route not found');
-  res.sendStatus(404);
-});
+// app.all('*', (req, res) => {
+//   console.log(req.path);
+//   console.log('route not found');
+//   res.sendStatus(404);
+// });
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router); // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 app.listen(PORT, () => {
   console.log(`Now listening on port ${PORT}`);
 });
 
+module.exports = app;
 module.exports.handler = serverless(app);
