e9ed9f3914882dec1fecd59fa4011f5a89545bb3
Corbin Taylor <cptaylor38@yahoo.com>
2020-05-15 16:45:34 -0400

    fixing netlify deploy

--- portfolio/src/Contact/Contact.jsx
+++ portfolio/src/Contact/Contact.jsx
@@ -27,7 +27,7 @@ const Contact = () => {
       try {
         setFormState(false);
         await axios
-          .post('.netlify/functions/sendGrid/contact/email', {
+          .post('/contact/email', {
             email: formData.email,
             subject: formData.subject,
             message: formData.message,
