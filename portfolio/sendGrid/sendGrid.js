9bd35826681e2331ffd617715561a2e04d637715
Corbin Taylor <cptaylor38@yahoo.com>
2020-05-15 18:17:40 -0400

    fnd

--- portfolio/sendGrid/sendGrid.js
+++ portfolio/sendGrid/sendGrid.js
@@ -12,17 +12,16 @@ const router = express.Router();
 
 router.post('/.netlify/functions/sendGrid/email', async (req, res) => {
   try {
-    res.json('req', req);
-    // const msg = {
-    //   to: 'cylor.dev@gmail.com',
-    //   from: 'cylor.dev@gmail.com',
-    //   subject: req.body.email,
-    //   text: req.body.message,
-    //   html: `<h3>${req.body.email}</h3>
-    //             <h4>${req.body.subject}</h4>
-    //             <p>${req.body.message}</p>`,
-    // };
-    // await sgMail.send(msg).then((response) => res.json('something', response));
+    const msg = {
+      to: 'cylor.dev@gmail.com',
+      from: 'cylor.dev@gmail.com',
+      subject: req.body.email,
+      text: req.body.message,
+      html: `<h3>${req.body.email}</h3>
+                <h4>${req.body.subject}</h4>
+                <p>${req.body.message}</p>`,
+    };
+    await sgMail.send(msg).then((response) => res.send(response));
   } catch (error) {
     console.log(error);
   }
