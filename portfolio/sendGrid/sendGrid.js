47993adb11f65b487865462ae45f18f1dcc56507
Corbin Taylor <cptaylor38@yahoo.com>
2020-05-15 18:20:42 -0400

    fnd

--- portfolio/sendGrid/sendGrid.js
+++ portfolio/sendGrid/sendGrid.js
@@ -8,23 +8,26 @@ const sgMail = require('@sendgrid/mail');
 const PORT = process.env.PORT || 5000;
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const router = express.Router();
-
-router.post('/.netlify/functions/sendGrid/email', async (req, res) => {
+app.post('/.netlify/functions/sendGrid/email', async (req, res) => {
   try {
-    const msg = {
-      to: 'cylor.dev@gmail.com',
-      from: 'cylor.dev@gmail.com',
-      subject: req.body.email,
-      text: req.body.message,
-      html: `<h3>${req.body.email}</h3>
-                <h4>${req.body.subject}</h4>
-                <p>${req.body.message}</p>`,
-    };
-    await sgMail.send(msg).then((response) => res.send(response));
+    res.send(req.body);
+    // const msg = {
+    //   to: 'cylor.dev@gmail.com',
+    //   from: 'cylor.dev@gmail.com',
+    //   subject: req.body.email,
+    //   text: req.body.message,
+    //   html: `<h3>${req.body.email}</h3>
+    //             <h4>${req.body.subject}</h4>
+    //             <p>${req.body.message}</p>`,
+    // };
+    // await sgMail.send(msg).then((response) => res.send(response));
   } catch (error) {
     console.log(error);
   }
+});
+
+app.get('/.netlify/functions/sendGrid/testing', (req, res) => {
+  res.send('wtf');
 });
 
 app.all('*', (req, res) => {
